014c61adb576da4340b1eae3142418e3ed290bb6
NguyenLoc25 <154305402+NguyenLoc25@users.noreply.github.com>
2024-12-02 19:54:00 +0700

    push

--- src/server.js
+++ src/server.js
@@ -4,5 +4,9 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "../public")));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 console.log('Server is running on port: ' + port);
